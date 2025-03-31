9310cc71e37c77b6d0d3b1ee7335fca60b04d1c3
Yejin-source <yejin3831@naver.com>
2025-03-31 15:35:28 +0900

    다양한 형태의 메서드 실습.. 과제 미완성

---
+++ java-oop/src/ch09/MethodEx2.java
@@ -0,0 +1,157 @@
+package ch09;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MethodEx2 {
+	// 반환타입: 숫자
+	// 매개타입: 배열 
+	// 배열.length API 사용 금지
+	// 배열을 입력하면 반환값으로 배열의 길이 반환
+	
+	public int m24a(int[] arr) { // ex) int x = new int[7]
+		int result = 0; // int result = arr.length -> 사용금지
+		
+		// foreach문 사용
+		for(int i : arr) { // arr의 개수만큼 i가 반복됨
+			result++;
+		}
+		return result;
+	}
+	
+	public int m24b(int[] arr) {
+		int i = 0; // arr.length 사용금지
+		boolean flag = true;
+		while(flag) { // 무한루프 -> flag가 true이기 때문에 계속 반복
+			try { // try절에서 예외가 발생하면 catch절로
+				int temp = arr[i];
+			} catch(Exception e) {
+				return i; // 예외 발생 시 현재 인덱스 반환 (배열 길이)
+			}
+			i++;
+		}
+ 		return i;
+	}
+	
+	
+	// Student타입을 디버깅한 문자열을 반환하는 메서드
+	// 반환타입: String
+	// 매개타입: 클래스 하나
+	public String m25a(Student s) {
+		String result = "";
+		result += "번호는 " + s.num + "이고, ";
+		result += "이름은 " + s.name + "입니다";
+		return result; // 번호는 ?이고, 이름은 ?입니다
+	}
+	
+	// 반환타입: boolean
+	// 매개타입: 클래스 두 개
+	public boolean m24c(Student s1, Student s2) {
+		boolean result = false;
+		
+		// s1, s2를 비교하는 코드
+		if(s1.num == s2.num && s1.name.equals(s2.name)) {
+			result = true;
+		}
+		return result;
+	}
+	
+
+	// 반환타입: 배열
+	// 매개타입: List
+	// List를 입력받아서 배열로 변경하여 반환
+	public int[] m4a(ArrayList<String> list) {
+		int[] result = new int[list.size()]; // 리스트 요소 개수 반환 (요소 추가/삭제 가능)
+		int i = 0;
+		for(String s : list) {
+			result[i] = Integer.parseInt(s);
+			i=i+1;
+		}		
+		return result;
+	}
+	
+	
+	// ! 과제 > 미완성
+	// 반환타입: 배열
+	// 매개타입: int
+	// 252 -> {2, 5, 2}
+	public int[] m5a(int num) { // 나누기 연산
+		int[] result = null;
+	/*
+		for(int i=0; i<result.length; i++) {
+			if(i == result.length) {
+				result[i] = result[i] / 10 * i;
+			}
+		}
+	*/	
+
+		return result;
+	}
+	
+	// 252 -> {"2", "5", "2"}
+	public String[] m5b(int num) { // substring() 메서드
+		String[] result = null;
+		String str = num+""; // "252" | 문자열로 바꾸기
+		result = new String[str.length()]; // new String[3]
+		for(int i=0; i<result.length; i++) {
+			if(i != result.length-1) { // 마지막 인덱스가 아니라면
+				result[i] = str.substring(i, i+1); // (0, 1) (1, 2)	
+			} else { // (2)
+				result[i] = str.substring(i); // 마지막 자리는 끝까지 가져옴
+			}
+		}	
+		return result;
+	}
+	
+	
+	// 반환타입: 클래스
+	// 입력타입: int, String
+	public Student m6a(int num, String name) {
+		Student s = new Student();
+		s.num = num;
+		s.name = name;
+		return s;
+	}
+	
+	
+	// 반환타입: Student[] 클래스의 배열
+	// 매개타입: List<Map>
+	public Student[] m7a(ArrayList<HashMap<String, Object>> list) {
+		Student[] arr = new Student[list.size()];
+		int i = 0;
+		for(HashMap<String, Object> m : list) {
+			arr[i] = new Student();
+			arr[i].num = (Integer)(m.get("num"));
+			arr[i].name = (String)(m.get("name"));
+			i=i+1;
+		}
+		return arr;
+	}
+	
+	/*
+	 * HashMap<String, Object>
+	 * String -> 키: 데이터를 구분하는 문자열 (ex. "num", "name")
+	 * Object -> 값: 어떤 데이터 타입이든 저장 가능
+	 */
+	
+	
+	// 반환타입: List<String>
+	// 매개타입: 임의의 개수의 문자열
+	public ArrayList<String> m8a(String...name) {
+		ArrayList<String> result = new ArrayList<String>();
+		for(String n : name) {
+			result.add(n);
+		}
+		return result;
+	}
+	
+	
+	// 반환타입: Map
+	// 매개타입: Student
+	public HashMap<String, Object> m9a(Student s) {
+		HashMap<String, Object> result
+			= new HashMap<String, Object>();
+		result.put("num", s.num);
+		result.put("name", s.name);
+		return result;
+	}
+}
---
+++ java-oop/src/ch09/MethodEx2Main.java
@@ -0,0 +1,108 @@
+package ch09;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MethodEx2Main {
+	public static void main(String[] args) { // 메소드 안에 this를 쓰는 경우 static 불가능
+		MethodEx2 m2 = new MethodEx2();
+		
+		// 반환타입: int
+		// 매개타입: 배열 
+		int[] arr = new int[7];
+		System.out.println(m2.m24a(arr)); //  7
+		System.out.println(m2.m24b(arr)); //  7
+		
+		
+		Student s1 = new Student();
+		s1.num = 2;
+		s1.name = "토게";
+		Student s2 = new Student();
+		s2.num = 3;
+		s2.name = "사토루";
+		Student s3 = new Student();
+		s3.num = 3;
+		s3.name = "사토루";
+		
+		
+		// Student 변수를 디버깅하는 메서드 (디버깅 < 로깅)
+		// 반환타입 String
+		// 매개타입 Student
+		System.out.println(s1.num + ", " + s1.name); // 이렇게 할 필요가 없어짐
+		System.out.println(m2.m25a(s1));
+		System.out.println(m2.m25a(s2));
+		
+		// Student 변수를 비교하는 메서드
+		System.out.println(s2 == s3); // false
+		System.out.println(s2.equals(s3)); // false
+		// 주소값이 다른 위치에 있기 때문에 false일 수밖에 없음
+		
+		System.out.println(m2.m24c(s2, s3)); // true
+		System.out.println(m2.m24c(s1, s2)); // false
+		
+		
+		ArrayList<String> list = new ArrayList<String>();
+			list.add("101");
+			list.add("200");
+			list.add("900");
+			int[] result = m2.m4a(list);
+			if(result != null) {
+				for(int n : result) {
+					System.out.println(n); // 101 200 900
+				}
+			}
+			
+			
+		int num = 252;
+		String[] result2 = m2.m5b(num);
+		for(String s : result2) {
+			System.out.println(s);
+		}
+		
+		
+		Student student = m2.m6a(8, "나기");
+		System.out.println(m2.m25a(student));
+		
+		
+		ArrayList<HashMap<String, Object>> list7 
+			= new ArrayList<HashMap<String, Object>>();
+		
+		HashMap<String, Object> map1
+			= new HashMap<String, Object>();
+		map1.put("num", 11);
+		map1.put("name", "바치라");
+		list7.add(map1);
+		
+		HashMap<String, Object> map2
+		= new HashMap<String, Object>();
+		map2.put("num", 21);
+		map2.put("name", "레이");
+		list7.add(map2);
+		
+		HashMap<String, Object> map3
+		= new HashMap<String, Object>();
+		map3.put("num", 7);
+		map3.put("name", "미야");
+		list7.add(map3);
+		
+		Student[] arr7 = m2.m7a(list7);
+		for(Student s : arr7) {
+			System.out.println(m2.m25a(s));
+		}
+		
+		
+		ArrayList<String> nameList
+			= m2.m8a("토게", "사토루", "메구미", "유타", "유우지");
+		for(String n : nameList) {
+			System.out.println(n);
+		}
+		
+		
+		Student s9 = new Student();
+		s9.num = 99;
+		s9.name = "켄마";
+		HashMap<String, Object> map9
+			= m2.m9a(s9);
+		System.out.println(map9.get("num"));
+		System.out.println(map9.get("name"));
+	}
+}
